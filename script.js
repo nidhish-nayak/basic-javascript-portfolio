150f4893151a44524061aa0ee2480dc69f42d85a
Nidhish <76598208+Nidhish-D-Nayak@users.noreply.github.com>
2022-02-07 23:31:08 +0530

    Release 2.0
    Changes made to website:
    1. Added a new svg path animation in hero page (updated)
    2. Added Projects page (2) with information
    3. Updates to navigation on click for several link and footer for hero page added with links (4)
    4. All files reduced size for faster loading
    5. Multi-device optimized completely. Even for Galaxy fold
    6. Added a meta description

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const navToggle = document.querySelector(".nav-toggle"); /*Variable for toggling the navigation bar*/
 const navLinks = document.querySelectorAll('.nav__link'); /*Variable for closing the nav when link is clicked*/
 const boxAlert = document.querySelectorAll(".box"); /*Variable for circle animation at intro*/
-const totheTop = document.getElementById("up-arrow"); /*Variable for To the top button display on scroll */
+const toTop = document.getElementById("up-arrow"); /*Variable for To the top button display on scroll */
+
+/*==========================
+Pre Loader
+==========================*/
+
+window.addEventListener('load', () => {
+  const preload = document.querySelector('.preload');
+  preload.classList.add('preload-finish');
+})
 
 /*========================== 
 Toggle Hamburger Icon 
@@ -25,7 +34,16 @@ ANIME JS ANIMATIONS
 ============================*/
 
 anime({
-  targets: '#svg-anime path',
+  targets: '#svg-anime .path-one',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutQuad',
+  duration: 10000,
+  direction: 'alternate',
+  loop: true
+});
+
+anime({
+  targets: '#svg-anime .path-two',
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutQuad',
   duration: 5000,
@@ -33,89 +51,9 @@ anime({
   loop: true
 });
 
-/*==================================================
-  Media Queries for the animated circle for ANIME JS
-==================================================*/
-
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    anime({
-      targets: '.box',
-      keyframes: [{
-          translateY: -40
-        },
-        {
-          translateX: 200
-        },
-        {
-          translateX: 0
-        },
-        {
-          translateY: 80
-        },
-        {
-          translateY: 0
-        }
-      ],
-      duration: 10000,
-      easing: 'easeOutElastic(1, 0.8)',
-      loop: true
-    });
-  } else {
-    anime({
-      targets: '.box',
-      keyframes: [{
-          translateY: -40
-        },
-        {
-          translateX: 350
-        },
-        {
-          translateX: 0
-        },
-        {
-          translateY: 180
-        },
-        {
-          translateY: 0
-        }
-      ],
-      duration: 10000,
-      easing: 'easeOutElastic(1, 0.8)',
-      loop: true
-    });
-
-  }
-}
-
-var x = window.matchMedia("(max-width: 560px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes this will be executed
-
-/*===========================================
-To the top display on scroll
-===========================================*/
-
-window.onscroll = function () {
-  scrollFunction()
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    totheTop.style.display = "block";
-  } else {
-    totheTop.style.display = "none";
-  }
-}
-
-
 /*===========================================
 Send E-Mail Using EmailJS
 ============================================*/
-
-// User ID: user_72V5ZrKiDqB9prAC597MF
-// Access Token: 2e1aaafd355adb16b7291a02519105fb
-
 
 // VALIDATION FUNCTION FOR EMPTY FIELDS
 
@@ -192,3 +130,79 @@ function inputEmpty() {
     button: "Close",
   });
 }
+
+/*===========================================
+To the top display on scroll
+===========================================*/
+
+window.onscroll = function () {
+  scrollFunctionOne()
+};
+
+function scrollFunctionOne() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+
+
+// /*==================================================
+//   Media Queries for the animated circle for ANIME JS
+// ==================================================*/
+
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//     anime({
+//       targets: '.box',
+//       keyframes: [{
+//           translateY: -40
+//         },
+//         {
+//           translateX: 200
+//         },
+//         {
+//           translateX: 0
+//         },
+//         {
+//           translateY: 80
+//         },
+//         {
+//           translateY: 0
+//         }
+//       ],
+//       duration: 10000,
+//       easing: 'easeOutElastic(1, 0.8)',
+//       loop: true
+//     });
+//   } else {
+//     anime({
+//       targets: '.box',
+//       keyframes: [{
+//           translateY: -40
+//         },
+//         {
+//           translateX: 350
+//         },
+//         {
+//           translateX: 0
+//         },
+//         {
+//           translateY: 180
+//         },
+//         {
+//           translateY: 0
+//         }
+//       ],
+//       duration: 10000,
+//       easing: 'easeOutElastic(1, 0.8)',
+//       loop: true
+//     });
+
+//   }
+// }
+
+// var x = window.matchMedia("(max-width: 560px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes this will be executed
